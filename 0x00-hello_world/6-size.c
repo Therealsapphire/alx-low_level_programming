10b809065dc24ca39d4f872cd30dd0d78cf80fb0
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-08 18:24:46 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc #include <stdio.h>
+#include <stdio.h>
 
 /**
  *  * main - main block
@@ -8,10 +8,10 @@ gcc #include <stdio.h>
  
 int main(void)
 {
-		printf("Size of a char: %i byte(s)\n", sizeof(char));
-			printf("Size of an int: %i byte(s)\n", sizeof(int));
-				printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %i byte(s)\n", sizeof(float));
+		printf("Size of a char: %i byte(s)\*n", sizeof(char));
+			printf("Size of an int: %i byte(s)\*n", sizeof(int));
+				printf("Size of a long int: %i byte(s)\*n", sizeof(long int));
+					printf("Size of a long long int: %i byte(s)\*n", sizeof(long long int));
+						printf("Size of a float: %i byte(s)\*n", sizeof(float));
 							return (0);
 }
