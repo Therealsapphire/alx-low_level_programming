1f8df33369aefe0b855d2cc0848f04e4171de6f7
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-09 21:44:19 -0700

    .

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#!/bin/bash
-#include «stdio.h>
+#include <stdio.h>
 /**
  * Description: main - Prints out a string to stdout
  * Return: 0 if success.
  */
 int main(void)
 {
-	printf("with proper grammar, but
-			he outcome is a piece of art, In"
-			return (0);
-			}
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
