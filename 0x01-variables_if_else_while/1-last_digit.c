f8d35016f47dc787dc624d519f1964908e652352
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-12 19:32:30 -0700

    .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <ctype.h>
 /**
  * main - program that prints the alphabet in lowercase, followed by a new line
  * You can only use the putchar
- * Return: 0
+ *  Return: 0
  */
 int main(void)
 {
 	int l = 'a';
 	while (l <= 'z')
-	{
-		putchar(l);
-		l += 1;
-	}
-	putchar('\n');
-	return (0);
+{
+	putchar(l);
+	l += 1;
 }
-	
+putchar('\n');
+return (0);
+}
