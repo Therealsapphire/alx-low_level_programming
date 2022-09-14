2adf210897680375c8b709054603785f44e1b9cc
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-14 00:48:24 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- *  * print_alphabet - Entry point
- *   * Return:0
- *    */
+* print_alphabet - Entry point
+* Return:0
+*/
 
 void print_alphabet(void)
 {
-		char c = 'a';
-
-			while (c <= 'z')
-					{
-								_putchar(c);
-										c++;
-
-											}
-				_putchar('\n');
-
+	char c = 'a';
+while (c <= 'z')
+{
+	_putchar(c);
+	c++;
+}
+_putchar('\n');
 }
