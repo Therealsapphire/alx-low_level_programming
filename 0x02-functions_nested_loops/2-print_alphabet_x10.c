115c4a59fd70749bbc2f486bfb578cc105703e86
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-14 00:55:15 -0700

    .

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first interger to be added.
+ *
+ * Return: The result of addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 +num2);
+}
