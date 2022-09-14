550cf8213efe739ec117b38984c0258bd2d77ff2
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-14 06:43:52 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,14 +12,14 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 	k = j * i;
-if (j == 0)
+else if (j == 0)
 {
 	_putchar(k + '0');
 }
-if (k < 10 && j != 0)
+else if (k < 10 && j != 0)
 {
 	_putchar(',');
-_putchar(' ');		
+	_putchar(' ');		
 	_putchar(' ');
 	_putchar(k + '0');
 }
