df3e1e54cd3c3781f5033bbeb99f0547c1df6cb4
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-15 22:31:03 -0700

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 {
 unsigned int num = n;
 
-			/*first check if its negative*/
+/*first check if its negative*/
 
 if (n < 0)
 
@@ -22,19 +22,15 @@ if (n < 0)
 
 											}
 
-
-
 /*print the first few digits*/
 
 if ((num / 10) > 0)
 
-							print_number(num / 10);
-
+print_number(num / 10);
 
 
 /*print the last digit*/
 
-					_putchar((num % 10) + 48);
+_putchar((num % 10) + 48);
 
 }
-
