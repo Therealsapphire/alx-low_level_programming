ba2641b8625fc93743765953fc44a04ec9efcd61
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-16 06:54:26 -0700

    .

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,24 +10,16 @@ double _sqrt(double x)
 {
 		float sqrt, tmp;
 
-			sqrt = x / 2;
+		sqrt = x / 2;
+		tmp = 0;
 
-				tmp = 0;
-
-					while (sqrt != tmp)
-
-							{
-
-										tmp = sqrt;
-
-												sqrt = (x / tmp + tmp) / 2;
-
-													}
-					return (sqrt);
-
+		while (sqrt != tmp)
+		{
+			tmp = sqrt;
+			sqrt = (x / tmp + tmp) / 2;
+		}
+		return (sqrt);
 }
-
-
 
 /**
  * largest_prime_factor - finds and prints the largest
@@ -37,33 +29,35 @@ double _sqrt(double x)
 
 void largest_prime_factor(long int num)
 {
-		int prmNu, largest;
+int prmNu, largest;
 
-			/* first divide with the smallest prime number (two) */
+/* first divide with the smallest prime number (two) */
+		while (num % 2 == 0)
 
-			while (num % 2 == 0)
+			num = num / 2;
 
-						num = num / 2;
+		/* num must be odd so we proceed to the next prime number (plus two) */
+		for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
+		{
+			while (num % prmNu == 0)
+			{
+				num = num / prmNu;
+				largest = prmNu;
+			}
+		}
 
+		if (num > 2)
+			largest = num;
+		printf("%d\n", largest);
+}
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	largest_prime_factor(612852475143);
 
-				/* num must be odd so we proceed to the next prime number (plus two) */
-
-				for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
-
-						{
-
-									while (num % prmNu == 0)												{
-
-																num = num / prmNu;
-
-																			largest = prmNu;
-
-																					}
-
-										}
-
-
-
-
-
+	return (0);
+}
