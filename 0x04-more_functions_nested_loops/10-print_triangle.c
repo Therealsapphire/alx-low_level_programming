a61cc89f7dc6d2ff6b6eea54f65f90f212d21ed5
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-15 22:45:41 -0700

    .

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,12 +8,13 @@
 
 void print_triangle(int size)
 {
-	int hght, base;
+int hght, base;
 
 	if (size <= 0)
 		_putchar('\n');
 	else
-	{for (hght = 1; hght <= size; ++hght)
+	{
+		for (hght = 1; hght <= size; ++hght)
 		{
 			for (base = 1; base <= size; ++base)
 			{
