a1f50c608d582450a70887905355af3e175ea13a
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-15 21:40:35 -0700

    .

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,25 +5,28 @@
  * @size: Is the size of the square.
  * Return: To the value of void..
  */
+
 void print_square(int size)
 {
 	int i;
+
 	int j;
-	i = 0;
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		while (i < size)
+	
+	i = 0
+		if (size <= 0)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			i++;
 			_putchar('\n');
 		}
-	}
+		else
+		{
+			while (i < size)
+			{
+				for (j = 0; j < size; j++)
+				{
+					_putchar('#');
+				}
+				i++;
+				_putchar('\n');
+			}
+		}
 }
