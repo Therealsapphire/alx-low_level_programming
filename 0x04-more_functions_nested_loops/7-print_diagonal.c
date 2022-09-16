4db3846725175f5b5141fd21201791b80c180220
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-15 21:09:22 -0700

    .

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,54 @@
 #include "main.h"
+
 /**
  * print_diagonal - Print a diagonal if the number is positive.
  * @n: The character to be count.
  * Return: To the value of c.
  */
+
 void print_diagonal(int n)
+
 {
+
 	int i;
-	int j:
-		if (n <= 0)
+
+	int j;
+
+	if (n <= 0)
+
+	{
+
+		_putchar('\n');
+
+	}
+
+	else
+
+	{
+
+		_putchar('\\');
+
+		_putchar('\n');
+
+		for (i = 0; i < (n - 1); i++)
+
 		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('\\');
-			_putchar('\n');
-			for (i = 0; i < (n - 1); i++)
+
+			for (j = i; j >= 0; j--)
+
 			{
-				for (j = i; j >= 0; j--)
-				{
-					_putchar(' ');
-				}
-				_putchar('\\');
-				_putchar('\n');
+
+				_putchar(' ');
+
 			}
+
+			_putchar('\\');
+
+			_putchar('\n');
+
 		}
+
+	}
+
 }
+
