95b0e9ddc10fbce48feda1d5816b759cded51493
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-15 21:32:24 -0700

    .

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,24 +8,22 @@
 void print_square(int size)
 {
 	int i;
-
 	int j;
-
-	i = 0
-		if (size <= 0)
+	i = 0;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < size)
 		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			i++;
 			_putchar('\n');
 		}
-		else
-		{
-			while (i < size)
-			{
-				for (j = 0; j < size; j++)
-				{
-					_putchar('#');
-				}
-				i++;
-				_putchar('\n');
-			}
-		}
+	}
 }
