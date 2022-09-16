796a9364f5ea9df010bb59811fcca41c5c59c152
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-16 06:28:37 -0700

    .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,48 +10,24 @@
  */
 
 int main(void)
-
 {
+	int num;
 
-		int num;
+	for (num = 1; num <= 100; ++num)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
 
-
-
-			for (num = 1; num <= 100; ++num)
-
-					{
-
-								if (num % 3 == 0 && !(num % 5 == 0))
-
-												printf("Fizz");
-
-										else if (num % 5 == 0 && !(num % 3 == 0))
-
-														printf("Buzz");
-
-												else if (num % 3 == 0 && num % 5 == 0)
-
-																printf("FizzBuzz");
-
-														else
-
-																		printf("%d", num);
-
-
-
-																if (num != 100)
-
-																				printf(" ");
-
-																		else
-
-																						printf("\n");
-
-																			}
-
-
-
-				return (0);
-
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
-
