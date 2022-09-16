5509681214b9cf3144f93031a49c772add5154a7
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-15 23:25:21 -0700

    .

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,12 @@ int num, row, count;
 for (row = 1; row <= 10; ++row)				{
 
 for (count = 0; count <= 14; ++count)
-
 										{
-										num = count;
+num = count;
 
 if (count > 9)																			{
-_putchar(1 + 48);																												num = count % 10;
+_putchar(1 + 48);
+num = count % 10;
 }
 _putchar(num + 48);
 
