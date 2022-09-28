013205132c1a43a33a8e461356eab68af1c27e0e
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-09-28 11:28:09 -0700

    .

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
@@ -14,5 +13,4 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (0);
 	return (x * _pow_recursion(x, y - 1));
-	return (-1);
 }
