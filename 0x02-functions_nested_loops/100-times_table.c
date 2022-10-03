ca7de4a513c82697d6b852d5225dc020a64b6a8f
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-10-02 23:43:42 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,12 +5,12 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int 0, j, k;
 if (n >= 0 && n <= 15)
 {
 for (j = 0; j <= n; j++)
 {
-	k = j * i;
+k = j * 0;
 if (j == 0)
 {
 	_putchar(k + '0');
