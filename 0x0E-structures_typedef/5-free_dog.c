3985465cd79b23cd54a92d467e4f33118f87df5c
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-10-10 11:00:43 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees the dogs.
+ * @d: The dogs to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
