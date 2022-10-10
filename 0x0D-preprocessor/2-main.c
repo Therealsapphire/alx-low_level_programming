1fb580563addbe164b4197ca61e658492a268885
Afolabi Timileyin <oluwatimilehinafolabi@gmail.com>
2022-10-10 08:35:56 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Main - Prints the name of the file program
+ * was compiled from, followed by a new line.
+ *
+ * Return: Always ).
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
